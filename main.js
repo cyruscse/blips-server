2aa9409dcd14de6031d4350f80ead18211b05db4
Cyrus Sadeghi <cyrus.cse@gmail.com>
2017-09-30 21:10:27 -0400

    Interfacing with MySQL, really basic still

--- main.js
+++ main.js
@@ -1,23 +1,45 @@
 const http = require('http');
 const fs = require('fs');
+const mysql = require('mysql');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+const sqlConnection = mysql.createConnection({
+	host      : 'localhost',
+	user      : 'root',
+	password  : 'pass',
+	database  : 'blips'
+});
 
 const server = http.createServer((req, res) => 
 {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
+
+	sqlConnection.connect();
 	
 	if (req.method == 'POST') 
 	{
 		console.log('POST');
+
+		var response = 'none';
+
+        sqlConnection.query('select * from Cities', function (error, results, fields) {
+        	if (error) throw error;
+        	console.log('First city is ', results[0].NAME);
+        	response = results[0].NAME;
+        });
+
+        sqlConnection.end();
+
 		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.end('post received');
+		res.end('post received ' + response);
 	}
 	else
 	{
 		console.log('GET');
+		sqlConnection.end();
 		//var html = fs.readFileSync('index.html');
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.end('get received');
