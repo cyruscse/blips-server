f271d27a6ec5ae1d4b349f4e1adcced691647cbd
Cyrus Sadeghi <cyrus.cse@gmail.com>
2017-11-23 16:38:09 -0500

    client_sync now retrieves city count from DB

--- modules/client_sync.js
+++ modules/client_sync.js
@@ -6,7 +6,9 @@ const attractionTypeQueryStr = "select * from AttractionTypes";
 
 // Logging Module setup
 const log_file = '/tmp/client_sync.log';
-var module_trace_level = logging.warning_level;
+var module_trace_level = logging.trace_level;
+
+var response;
 
 function log (entry_trace_level, entry) {
     logging.log(entry_trace_level, module_trace_level, log_file, entry);
@@ -16,8 +18,17 @@ function setModuleTraceLevel (newLevel) {
     module_trace_level = newLevel;
 }
 
-exports.sync = (response, jsonRequest) => {
-	log(logging.critical_level, "received DBSYNC request");
+function cityCountCallback(results, callerCallback, queryArgs) {
+	var key = (Object.keys(results[0])[0]);
+
+	log(logging.trace_level, 'DBSYNC request got ' + results[0][key]);
 
 	response.end();
+}
+
+exports.sync = (httpResponse, jsonRequest) => {
+	log(logging.trace_level, "received DBSYNC request");
+	response = httpResponse;
+
+	mySQLClient.queryAndCallback(cityCountQueryStr, cityCountCallback, null, null);
 }
